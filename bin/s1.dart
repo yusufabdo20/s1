16009799ed4c38be5ae3e9c05b2310a5370c2b06
yusufafatah@gmail.com <yusufabdo20>
2026-04-01 12:28:28 +0200

    Initialize project structure with essential files including .gitignore, README, CHANGELOG, and analysis options. Implement basic functionality in main application and add initial test cases.

---
+++ bin/s1.dart
@@ -0,0 +1,175 @@
+void main() {
+// print("Hello ");
+// int a = 5 ; 
+// print("a= $a" );
+// print("a > 5 ->  ${a>5}") ; 
+// print("a == 5 ->  ${a==5}") ; 
+// print("a != 5 ->  ${a!=5}") ; 
+// print("a >= 5 ->  ${a>=5}") ; 
+// print("a <= 5 ->  ${a<=5}") ; 
+// print("a < 5 ->  ${a<5}") ; 
+
+// Person - > 18+ , hasLicense
+// int age  = 20 ;  
+// bool hasLicense = true ;
+// bool hasCar = true ;
+// bool canDrive = age>=18 && hasLicense && hasCar ; //(true , true , true) => true
+// print("age -> $age , isAge >= 18 -> ${age>=18}, hasLicense -> $hasLicense") ;
+// print("Can drive ?  -> $canDrive") ;
+
+// bool isMember = false ; 
+// bool isFirstTimeCustomer = false ;
+// bool hasCoupon= true ; 
+
+// bool hasDisc =(isFirstTimeCustomer || hasCoupon) && isMember  ; 
+// print("isMember -> $isMember , isFirstTimeCustomer -> $isFirstTimeCustomer , hasCoupon -> $hasCoupon") ;
+// print("Has discount ? -> $hasDisc") ;
+
+// bool isInRoom = false ; 
+// if(isInRoom){
+//   print("Turn On"); 
+// }
+// else{
+//   print("Turn Off");}
+ /*
+ if(condition)
+ {
+ //code  if condition 
+ }
+ else{
+ //code if not 
+ }
+
+  */
+//  int score =55;  
+// if(score > 90) { 
+//   print("A"); 
+// }
+// else if(score > 80) {
+//   print("B");
+// }
+// else if(score > 70) {
+//   print("C");
+// }
+// else if(score > 60) {
+//   print("D");
+// }
+// else {
+//   print("F");
+
+// }
+
+// int age = 20 ;
+// bool hasTicket  = false ;
+// if(age >= 18 ){ 
+//   print("AGE > = 18 ") ; 
+//   if(hasTicket){
+//     print("Has Access") ; 
+//   }
+//   else{
+//     print("You can buy ticket") ; 
+//   }
+// }else{
+//   print("No Access") ; 
+// }
+
+// String username = "admin" ;
+// String password = "1234" ;
+
+// if(username == "admin" && password =="1234") {
+//   print("Success") ; 
+// }else{
+//   print("Error") ;
+//  }
+
+/**
+ * switch (variable){
+ * case value 1  :
+ *    code ; 
+ *    break ; 
+ * case value 2  :
+ *    code ; 
+ *    break ; 
+ * case value 3  :
+ *    code ; 
+ *    break ; 
+ * 
+ * 
+ * }
+ * 
+ * 
+ */
+String status = "can" ;
+
+// switch (status){
+//   case "pendding" :
+//     print("Your case in Pendding State , Wait ! "); 
+//     break ;
+//    case "approve" :
+//     print("Your case is Approved  , Success ! "); 
+//     break ;
+//    case "reject" :
+//    case "cancel":
+//     print("Your case is Rejected  , Sorry ! "); 
+//     break ;
+//    case "inprogress" :
+//     print("Your case in progress "); 
+//     break ;
+//     default:
+//     print("SORRY  ,, THERE is ERROR") ; 
+      
+// }
+// int score = 84 ; 
+// switch (score) {
+//   case >90 :
+//   print("A"); 
+//   break ; 
+//   case >80 :
+//   int a = 4  ; 
+//   int b = 8 ; 
+//   print(a+b); 
+//   print("B");  
+//   break ; 
+//   case >70 :
+//   print("C"); 
+//   break ; 
+//   case >50 :
+//   print("D"); 
+//   break ; 
+// default:
+// print("F") ; 
+// }
+
+/// for 
+/// for (;;) ;
+
+/// for (;;){
+/// code}
+
+/// for(;condition;){
+/// code}
+/// 
+// int score = 1 ;
+// for (int score = 1 ;score<5;){
+//   print("YUSUF") ; 
+// }
+
+/// for(initialization;condition;){
+/// code}
+/// 
+// for (int score = 1 ;score<5;){
+//   print("YUSUF") ; 
+//   score = score +1 ; 
+// }
+/// for(initialization;condition;increment){
+/// code}
+/// 
+// for (int score = 1 ;score<11; score++){
+//   print("YUSUF") ; 
+// }
+
+for (int i = 1 ; i<= 10 ;i++ ){ 
+  print(i); 
+}
+}
+
---
+++ test/s1_test.dart
@@ -0,0 +1,8 @@
+import 'package:s1/s1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
