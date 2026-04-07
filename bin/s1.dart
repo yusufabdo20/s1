e60268d59b6f0e7e094ee5b8d0aa5c35c41d3302
yusufabdo20 <yusufafatah@gmail.com>
2026-04-07 18:31:58 +0200

    Refactor main function and implement Student and StudentManager classes with student listing functionality

--- bin/s1.dart
+++ bin/s1.dart
@@ -1,175 +1,126 @@
-void main() {
-// print("Hello ");
-// int a = 5 ; 
-// print("a= $a" );
-// print("a > 5 ->  ${a>5}") ; 
-// print("a == 5 ->  ${a==5}") ; 
-// print("a != 5 ->  ${a!=5}") ; 
-// print("a >= 5 ->  ${a>=5}") ; 
-// print("a <= 5 ->  ${a<=5}") ; 
-// print("a < 5 ->  ${a<5}") ; 
+void main(){
 
-// Person - > 18+ , hasLicense
-// int age  = 20 ;  
-// bool hasLicense = true ;
-// bool hasCar = true ;
-// bool canDrive = age>=18 && hasLicense && hasCar ; //(true , true , true) => true
-// print("age -> $age , isAge >= 18 -> ${age>=18}, hasLicense -> $hasLicense") ;
-// print("Can drive ?  -> $canDrive") ;
 
-// bool isMember = false ; 
-// bool isFirstTimeCustomer = false ;
-// bool hasCoupon= true ; 
+List<Student> students = [
+    Student("Ahmed", 20, 2.9) , 
+    Student("Yusuf", 25, 3.2) , 
+    Student("Ali", 20, 2.9) , 
+    Student("Mohamed", 20, 2.9) , 
+    Student("Sarah", 20, 2.9) , 
+    Student("Fatima", 20, 2.9) , 
+    Student("Aisha", 20, 2.9) , 
+] ; 
+List<Student> students2 = [
+    Student("Ahmed", 20, 2.9) , 
+    Student("Yusuf", 25, 3.2) , 
+    Student("Ali", 20, 2.9) , 
+    Student("Mohamed", 20, 2.9) , 
+    Student("Sarah", 20, 2.9) , 
+    Student("Fatima", 20, 2.9) , 
+    Student("Aisha", 20, 2.9) , 
+] ; 
 
-// bool hasDisc =(isFirstTimeCustomer || hasCoupon) && isMember  ; 
-// print("isMember -> $isMember , isFirstTimeCustomer -> $isFirstTimeCustomer , hasCoupon -> $hasCoupon") ;
-// print("Has discount ? -> $hasDisc") ;
+StudentManager manager = StudentManager("Ali") ;
+manager.students = students ;
+manager.listAllStudents() ; 
 
-// bool isInRoom = false ; 
-// if(isInRoom){
-//   print("Turn On"); 
-// }
-// else{
-//   print("Turn Off");}
- /*
- if(condition)
- {
- //code  if condition 
- }
- else{
- //code if not 
- }
+StudentManager manager2 = StudentManager("Khaled") ;
+manager2.students = students2 ;
+manager2.listAllStudents() ; 
 
-  */
-//  int score =55;  
-// if(score > 90) { 
-//   print("A"); 
-// }
-// else if(score > 80) {
-//   print("B");
-// }
-// else if(score > 70) {
-//   print("C");
-// }
-// else if(score > 60) {
-//   print("D");
-// }
-// else {
-//   print("F");
+// List<int> ages = [9,8,9,6,9,8,7,9,3,3,9,4,] ; 
+// print (ages) ; 
 
-// }
+// List<String> cart = ['burger' , 'fries' , 'cola' ] ; 
+// print(cart) ; 
+// print(cart[1]); 
+// print(cart.length) ;
 
-// int age = 20 ;
-// bool hasTicket  = false ;
-// if(age >= 18 ){ 
-//   print("AGE > = 18 ") ; 
-//   if(hasTicket){
-//     print("Has Access") ; 
-//   }
-//   else{
-//     print("You can buy ticket") ; 
-//   }
-// }else{
-//   print("No Access") ; 
+
+// List<int> ids =[232,323,3232,4,5,55,5,5,5,0,9,95,232] ; 
+// print(ids);
+// Set<int> uniqueIds = {232,323,3232,4,5,55,5,5,5,0,9,95,232};
+// print (uniqueIds); 
+// uniqueIds.add(88); 
+// uniqueIds.add(5);
+// print(uniqueIds);
+//Map 
+// Map<String, int> prices = {
+//     "burger" : 100 , 
+//     "fries" : 30 , 
+//     "cola" : 15 ,
+// } ; 
+// print(prices) ; 
+// print(prices['burger']) ; 
+
+// showInfo(2.6 , "Ahmed") ; 
+
+// int p = sum (4, 2) * 9 ; 
+// print (p); 
+// fo (init ; condition ; inc) {
+//     code ; 
 // }
 
-// String username = "admin" ;
-// String password = "1234" ;
-
-// if(username == "admin" && password =="1234") {
-//   print("Success") ; 
-// }else{
-//   print("Error") ;
-//  }
-
-/**
- * switch (variable){
- * case value 1  :
- *    code ; 
- *    break ; 
- * case value 2  :
- *    code ; 
- *    break ; 
- * case value 3  :
- *    code ; 
- *    break ; 
- * 
- * 
- * }
- * 
- * 
- */
-String status = "can" ;
-
-// switch (status){
-//   case "pendding" :
-//     print("Your case in Pendding State , Wait ! "); 
-//     break ;
-//    case "approve" :
-//     print("Your case is Approved  , Success ! "); 
-//     break ;
-//    case "reject" :
-//    case "cancel":
-//     print("Your case is Rejected  , Sorry ! "); 
-//     break ;
-//    case "inprogress" :
-//     print("Your case in progress "); 
-//     break ;
-//     default:
-//     print("SORRY  ,, THERE is ERROR") ; 
-      
+// while (condition) {
+//     code ; 
 // }
-// int score = 84 ; 
-// switch (score) {
-//   case >90 :
-//   print("A"); 
-//   break ; 
-//   case >80 :
-//   int a = 4  ; 
-//   int b = 8 ; 
-//   print(a+b); 
-//   print("B");  
-//   break ; 
-//   case >70 :
-//   print("C"); 
-//   break ; 
-//   case >50 :
-//   print("D"); 
-//   break ; 
-// default:
-// print("F") ; 
+// int i = 0 ; 
+// while (i<5){
+// print("Yusuf") ; 
+// i++ ; 
 // }
 
-/// for 
-/// for (;;) ;
+// do {
+// print(i);
+// i++ ; 
+// }while(i<5);
 
-/// for (;;){
-/// code}
 
-/// for(;condition;){
-/// code}
-/// 
-// int score = 1 ;
-// for (int score = 1 ;score<5;){
-//   print("YUSUF") ; 
-// }
-
-/// for(initialization;condition;){
-/// code}
-/// 
-// for (int score = 1 ;score<5;){
-//   print("YUSUF") ; 
-//   score = score +1 ; 
-// }
-/// for(initialization;condition;increment){
-/// code}
-/// 
-// for (int score = 1 ;score<11; score++){
-//   print("YUSUF") ; 
-// }
-
-for (int i = 1 ; i<= 10 ;i++ ){ 
-  print(i); 
 }
+// type functionName (){
+//code ; 
+//} 
+// void showInfo (double gpa , String name) {
+// print("Hello , $name") ; 
+// print ("Your GPA : $gpa ") ; 
+// print ("You teach Dart Lang") ;  
+// }
+
+// int sum (int x , int y ) {
+//     int z = x+y ; 
+//     return z ; 
+// }
+
+class Student {
+    String? name; 
+    int? age  ; 
+    double? gpa ;
+Student (String name1 , int age1 , double gpa1 ) {
+    name = name1 ; 
+    age = age1 ; 
+    gpa = gpa1; 
+}
+//method
+void showInfo (){
+    print("Name : $name  ,  Age : $age , GPA : $gpa" ); 
 }
 
+}
+class StudentManager {
+   
+    String? name ; 
+    List<Student> students=[] ; 
+    StudentManager(String name1){
+        name = name1 ; 
+    } 
+    void listAllStudents (){
+        if(students.isEmpty){
+            print("No students yet") ;
+        }
+        else{
+            for (int i =0 ; i<students.length ; i++){
+                students[i].showInfo(); 
+            }
+        }
+    }
+}
